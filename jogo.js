5f58440ca68350a4c0078b064d84e56497c70526
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-06-21 20:14:54 -0300

    Aplicando nível de dificuldade

--- jogo.js
+++ jogo.js
@@ -4,6 +4,22 @@ var largura = 0;
 var vidas = 1;
 var tempo = 10;
 
+var criaMosquitoTempo = 1500;
+
+var nivel = window.location.search;
+nivel = nivel.replace('?', '');
+
+if (nivel === 'normal') {
+  //1500
+  criaMosquitoTempo = 1500;
+} else if (nivel === 'dificil') {
+  //1000
+  criaMosquitoTempo = 1000;
+} else if (nivel === 'chuck_norris') {
+  //750
+  criaMosquitoTempo = 750;
+}
+
 function ajustaTamanhoPalcoJogo() {
   altura = window.innerHeight
   largura = window.innerWidth
